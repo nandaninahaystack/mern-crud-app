897a1fe6d93e8a9f61563f8754475d3dfc52c3a9
Nandan Ray <nandan@haystackanalytics.in>
2022-02-01 19:56:18 +0530

    DB Error Fix

--- api/server.js
+++ api/server.js
@@ -7,45 +7,55 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect('mongodb://127.0.0.1:27017/mern-todo', {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true
 })
-    .then(() => console.log("Connected to DB"))
-    .catch(console.error);
+	.then(() => console.log("Connected to MongoDB"))
+	.catch(console.error);
 
 const Todo = require('./models/Todo');
 
 app.get('/todos', async (req, res) => {
-    const todos = await Todo.find();
+	const todos = await Todo.find();
 
-    res.json(todos);
+	res.json(todos);
 });
 
 app.post('/todo/new', (req, res) => {
-    const todo = new Todo({
-        text: req.body.text
-    });
-    
-    todo.save();
+	const todo = new Todo({
+		text: req.body.text
+	})
 
-    res.json(todo);
+	todo.save();
+
+	res.json(todo);
 });
 
 app.delete('/todo/delete/:id', async (req, res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id);
+	const result = await Todo.findByIdAndDelete(req.params.id);
 
-    res.json(result);
-})
+	res.json({result});
+});
 
 app.get('/todo/complete/:id', async (req, res) => {
-    const todo = await Todo.findById(req.params.id);
+	const todo = await Todo.findById(req.params.id);
 
-    todo.complete = !todo.complete;
+	todo.complete = !todo.complete;
 
-    todo.save();
+	todo.save();
 
-    res.json(todo);
+	res.json(todo);
 })
+
+app.put('/todo/update/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.text = req.body.text;
+
+	todo.save();
+
+	res.json(todo);
+});
 
 app.listen(3001, () => console.log("Server started on port 3001"));
